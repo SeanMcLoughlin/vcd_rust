a563dfecaf8074be097f191fd3dcf4e3c89cbfc7
Sean McLoughlin <smcloughlin1015@gmail.com>
2020-09-19 18:57:05 -0700

    Add integration tests

---
+++ tests/file_parsing_integration_test.rs
@@ -0,0 +1,31 @@
+use std::path::PathBuf;
+use vcd_rust;
+
+fn get_test_file_path(filename: &str) -> String {
+    let mut test_file_buf = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    test_file_buf.push(format!("tests/{}", filename));
+    return test_file_buf.as_path().display().to_string();
+}
+
+#[test]
+fn parse_vcd_file() {
+    let test_file = get_test_file_path("no_vardump.golden.vcd");
+    let vcd = vcd_rust::vcd::VCDLoader::load_from_file(test_file).unwrap();
+
+    assert_eq!(vcd.date, "August 9th, 2020");
+    assert_eq!(vcd.version, "Version 4.20");
+    assert_eq!(
+        vcd.comments,
+        vec![
+            "The golden VCD test file with no dumped vars",
+            "Here's another comment for good measure"
+        ]
+    );
+    assert_eq!(
+        vcd.timescale,
+        vcd_rust::types::TimeScale {
+            value: 1,
+            unit: vcd_rust::types::TimeUnit::PS
+        }
+    )
+}
